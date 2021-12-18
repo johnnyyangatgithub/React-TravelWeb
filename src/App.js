bd8ff0bf9c1b84f2558e6ae72417a85ffebdba33
Johnny <yuhangyang74@gmail.com>
2021-12-18 15:38:21 +0800

    This is the first commit of my React application

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
